c96923fdbaa05f806443445076b5d48462f523ea
swrhim <swrhim@gmail.com>
2014-09-15 22:04:33 -0400

    Initial Commit
    First run at the proxy library

---
+++ ProxyLibrary/Connection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ProxyLibrary
+{
+    public class Connection
+    {
+        private Socket _socket = null;
+        
+        public Connection(Socket socket)
+        {
+            _socket = socket;
+        }
+
+
+    }
+}
---
+++ ProxyLibrary/Proxy.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyLibrary
+{
+    public class Proxy
+    {
+        private TcpListener _tcpListener = null;
+        int _port = 9999;
+        private IPAddress _localAddress = IPAddress.Parse("127.0.0.1");
+
+        public int Port
+        {
+            get { return _port; }
+            set { _port = value; }
+        }
+
+        public IPAddress IPAddress
+        {
+            get { return _localAddress }
+            set { _localAddress = value; }
+        }
+
+        public Proxy()
+        {
+            _tcpListener = new TcpListener(_localAddress, _port);
+        }
+
+        public Proxy(int port, IPAddress address)
+        {
+            _port = port;
+            _localAddress = address;
+            _tcpListener = new TcpListener(_localAddress, _port);
+        }
+
+        public Proxy(int port)
+        {
+            _port = port;
+            _tcpListener = new TcpListener(IPAddress.Any, port);
+        }
+
+        public void StartServer()
+        {
+            _tcpListener.Start();
+        }
+
+        public void AcceptConnection(Connection conn)
+        {
+            
+        }
+
+        public Socket GetSocket()
+        {
+            if (_tcpListener != null)
+                return _tcpListener.AcceptSocket();
+            return null;
+        }
+
+    }
+}
